9e8f181614d60a4f09f795c42cf2bc3e015a8a09
Wilker Lucio <wilkerlucio@gmail.com>
2014-07-14 17:17:10 +0200

    display flex mixin

--- src/mower/core.clj
+++ src/mower/core.clj
@@ -1,11 +1,30 @@
 (ns mower.core
-  (:require [clojure.walk :as walk]))
+  (:require [clojure.walk :as walk]
+            [clojure.set :as set]))
+
+(defn vendorize [v type]
+  (if (= type :official)
+    v
+    (str "-" (name type) "-" v)))
+
+(defn vendor-value [value & {:keys [only]}]
+  (let [all-vendors #{:webkit :ms :moz :o :official}
+        included (if only (set/intersection all-vendors only) all-vendors)]
+    (into #{} (map (partial vendorize value) included))))
 
 (defmulti mixin
   (fn [[k _]] (keyword k))
   :default ::default)
 
 (defmethod mixin ::default [[k v]] {k v})
+
+(defmethod mixin :display [[k v]]
+  (if (#{"flex" "inline-flex"} v)
+    (let [box (if (= v "flex") (into #{"-ms-flexbox"} (vendor-value "box" :only #{:moz :webkit :official}))
+                               (into #{"-ms-inline-flexbox"} (vendor-value "inline-box" :only #{:moz :webkit :official})))
+          flex (vendor-value v :only #{:webkit :official})]
+      {k (into (sorted-set) (set/union box flex))})
+    {k v}))
 
 (defn apply-mixins [definitions]
   (into {} (mapcat mixin definitions)))
--- test/mower/core_test.clj
+++ test/mower/core_test.clj
@@ -7,6 +7,27 @@
 (defmethod mixin :sample-expand [[k v]] {k v
                                          (str "-moz-" (name k)) v})
 
+(deftest vendorize-test
+  (testing "vendorizing third part"
+    (is (= (vendorize "sample" "moz") "-moz-sample"))
+    (is (= (vendorize "sample" :webkit) "-webkit-sample")))
+  (testing "vendorizing official"
+    (is (= (vendorize "sample" :official) "sample"))))
+
+(deftest vendor-value-test
+  (testing "vendorizing value"
+    (is (= (vendor-value "flex") #{"-webkit-flex" "-ms-flex" "-moz-flex" "-o-flex" "flex"}))
+    (is (= (vendor-value "flex" :only #{:webkit :official}) #{"-webkit-flex" "flex"}))))
+
+(deftest mixin-display-flex-test
+  (testing "doesn't change for regular displays"
+    (is (= (mixin [:display "block"]) {:display "block"}))
+    (is (= (mixin [:display "none"]) {:display "none"})))
+  (testing "converting flex"
+    (is (= (mixin [:display "flex"]) {:display (sorted-set "-webkit-box" "-moz-box" "-webkit-flex" "-ms-flexbox" "box" "flex")})))
+  (testing "converting inline-flex"
+    (is (= (mixin [:display "inline-flex"]) {:display (sorted-set "-webkit-inline-box" "-moz-inline-box" "-webkit-inline-flex" "-ms-inline-flexbox" "inline-box" "inline-flex")}))))
+
 (deftest apply-mixins-test
   (let [sample {:sample "value"}]
     (testing "applying no filters"
