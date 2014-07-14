e469160fb328f196554f343aab2276e77abd6921
Wilker Lucio <wilkerlucio@gmail.com>
2014-07-14 17:20:35 +0200

    updated license

--- README.md
+++ README.md
@@ -1,14 +1,7 @@
-# mower
+# Mower
 
-A Clojure library designed to ... well, that part is up to you.
+A Clojure library designed that can pre-process your CSS for goodies.
 
 ## Usage
 
-FIXME
-
-## License
-
-Copyright © 2014 FIXME
-
-Distributed under the Eclipse Public License either version 1.0 or (at
-your option) any later version.
+Check later...
--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject mower "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "CSS Pre-processing framework"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "MIT"
+            :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]])
